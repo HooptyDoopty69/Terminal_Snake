92be5b080b7ee8c82393293524226ba35e1503ff
Aron Kvaszta <aronkvaszta012@gmail.com>
2023-05-12 13:46:31 +0200

    Added game board creation

--- snake.py
+++ snake.py
@@ -3,16 +3,30 @@ from colorama import Fore, init
 from pytimedinput import timedKey
 
 
+GAME_WIDTH = 16
+GAME_HEIGHT = 32
+
 
 ####################
 # GAME BOARD STUFF #
 ####################
+board = [(col, row) for col in range(GAME_WIDTH)
+         for row in range(GAME_HEIGHT)]
+
+wall_sign = Fore.YELLOW + '#'
 
 
+def draw_board():
+    for cell in board:
+        if cell[1] == GAME_HEIGHT - 1:
+            print('')
+        elif cell[0] == 0 or cell[0] == GAME_WIDTH - 1 or cell[1] == 0 or cell[1] == GAME_HEIGHT - 2:
+            print(wall_sign, end='')
+        else:
+            print(' ', end='')
 
 
 ##########################
-
 
 
 #############
@@ -29,16 +43,18 @@ def main():
     while not should_close:
         # Get all current inputs
         # Reset the console on timeout
-        # Only register accepted keys 
-        pressed_key, _ = timedKey(f'Q - quit\nWASD - Up/Left/Down/Right\nPressed key: {pressed_key}', timeout=0.3, resetOnInput=True, allowCharacters="qwasd")
-        
+        # Only register accepted keys
+        pressed_key, _ = timedKey(
+            f'Q - quit\nWASD - Up/Left/Down/Right\nPressed key: {pressed_key}', timeout=0.3, resetOnInput=True, allowCharacters="qwasd")
+
         # Stops the game loop if escape character is pressed
         if pressed_key == 'q':
             should_close = True
         # Redraws the screen on every timeout
         else:
             system("\033[H")
-            print('hello')
+            draw_board()
+
 
 if __name__ == "__main__":
     main()
